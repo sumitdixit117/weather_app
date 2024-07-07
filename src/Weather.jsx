695c08ac1c3e699019de87bd66784489ca40c22e
Shashwat Dixit <dixitsumit117@gmail.com>
2024-07-08 01:10:16 +0530

    Updating

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -7,7 +7,7 @@ const Weather = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=941754e617a6fd538c1880ce5d76f829`;
 
       let res = await fetch(url);
       let data = await res.json();
